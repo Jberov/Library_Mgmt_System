1a392ccf52ef41653e6793d339969afaef381397
I551610 <yordan.berov@sap.com>
2023-08-04 00:53:49 +0300

    Add book page - basic

---
+++ Spring/src/main/Frontend/bootstrap-5-categories-template-main/js/AddBookScript.js
@@ -0,0 +1,36 @@
+function validateInput(){
+
+}
+
+function createJSONPayload() {
+    let jsonObj = new Object();
+    jsonObj.isbn = $("#isbn").val();
+    jsonObj.count = parseInt($("#count").val());
+    jsonObj.author = $("#author").val();
+    jsonObj.authorDescription = $("#authorDesc").val();
+    jsonObj.name = $("#bookName").val();
+    jsonObj.genre = $("#Genre").val();
+    jsonObj.genreDescription = $("#GenreDesc").val();
+    jsonObj.description = $("#description").val();
+    return JSON.stringify(jsonObj);
+}
+
+$(document).ready(async function(){
+
+    $("button").click(function(){
+        $.ajax({
+            url: 'http://localhost:8080/api/v1/books',
+            type: 'POST',
+            data: createJSONPayload(),
+            contentType: 'application/json; charset=utf-8',
+            dataType: 'text',
+            async: true,
+            success: function(result) {
+               alert(result.message);
+            },
+            error: function(result) {
+                alert("Грешка при операцията")
+            }
+        });
+    });
+})
